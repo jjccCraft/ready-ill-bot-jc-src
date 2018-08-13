c1e36c11999190751dbf29e5e5cde3a2d14abc59
Joshua Chalcraft <t-jochal@microsoft.com>
2018-08-13 13:27:47 +0100

    First Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=ready-ill-bot-jc-Web-Deploy.pubxml -p:Password=7ETpth04gJgluxBoCCg3vkwyYtoDETNN3XDZPnCFKhjrobfducq4HajjX8EC
+
